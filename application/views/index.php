3e7f69a6a693e7eb88c09fc4ebab0f4f50550f3f
Vitaliy Serov <serovvitaly@gmail.com>
2013-01-23 23:21:35 +0400

    мелкие изменения

--- application/views/index.php
+++ application/views/index.php
@@ -4,8 +4,32 @@
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
   <title>Untitled</title>
   <link rel="stylesheet" type="text/css" href="/skin/css/style.css">
+  
+  <script src="/vendor/jquery/jquery-1.9.0.min.js"></script>
+  
+  <script src="/vendor/microfiche/microfiche.js"></script>
+  <link rel="stylesheet" href="/vendor/microfiche/microfiche.css"></link> 
+  
+  <script>
+  $(document).ready(function(){
+      $('#main-content').microfiche();
+  });
+  </script>
+  
 </head>
 <body>
+  <header class="header">
+  
+  </header>
+  <div class="example" id="main-content">
+    <div>
+      <ul>
+        <li><div class="slide-content">1</div></li>
+        <li><div class="slide-content">2</div></li>
+        <li><div class="slide-content">3</div></li>
+      </ul>
+    </div>
+  </div>
   
 </body>
 </html>
